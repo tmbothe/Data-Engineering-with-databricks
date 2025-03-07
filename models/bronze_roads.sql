be155af4898fc870c4500ca4a8b8799c3571d2be
Thimothe Konchou <mbothe7@gmail.com>
2025-03-07 14:05:53 +0000

    added sources for traffic and rowds data

--- models/bronze_roads.sql
+++ models/bronze_roads.sql
@@ -1,2 +1,10 @@
-SELECT *
-FROM {{source('landing','raw_roads')}}
+SELECT 'Road ID' as Road_ID,
+       'Road category id' as Road_Category_Id,
+       'Road category' as Road_Category,
+       'Region id' as Region_ID ,
+       'Region name' as Region_Name,
+       'Total link length km' as Total_Link_Length_Km,
+       'Total link length miles' as Total_Link_Length_Miles,
+       'All motor vehicles' as All_Motor_Vehicles ,
+       current_timestamp() as create_at
+FROM {{ source('landing','raw_roads') }}
